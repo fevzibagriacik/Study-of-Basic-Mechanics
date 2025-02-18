6ba06c427af4bd12e7ed72910f987c4ba8e894bb
Fevzi Bağrıaçık <fevzi.bagriacik1905@gmail.com>
2025-02-18 15:18:04 +0300

    WallJump and WallSlide are added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,27 @@ public class PlayerController : MonoBehaviour
     private float moveInput;
     public float jumpForce;
     public float checkRadius = 0.3f;
+    public float wallCheckRadius = 0.3f;
+    public float wallJumpForce = 10f;
+    public float wallSlidingSpeed = 0.2f;
+    public float wallJumpingDirection;
+    public float wallJumpingTime = 0.2f;
+    public float wallJumpingCounter;
+    public float wallJumpingDuration = 0.4f;
 
     [SerializeField] int jumpCount = 0;
 
-    private bool isGrounded;
+    [SerializeField] bool isGrounded;
+    [SerializeField] bool isWallSliding;
+    [SerializeField] bool isWallJumping;
 
     public Transform groundCheck;
+    public Transform wallCheck;
 
     public LayerMask groundLayer;
+    public LayerMask wallLayer;
+
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
     private void Start()
     {
@@ -38,6 +51,10 @@ public class PlayerController : MonoBehaviour
         Jump();
 
         DoubleJump();
+
+        WallSlide();
+
+        WallJump();
     }
 
     public void Move()
@@ -63,9 +80,55 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void WallSlide()
+    {
+        if(rb.velocity.y < 0 && !isGrounded && IsWalled())
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    public void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingCounter = wallJumpingTime;
+            CancelInvoke("StopWallJumping");
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            wallJumpingCounter = 0f;
+            Invoke("StopWallJumping", wallJumpingDuration);
+        }
+    }
+
+    public void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
     public bool IsGrounded()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
         return isGrounded;
+    }
+
+    public bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, wallLayer);
     }
 }
