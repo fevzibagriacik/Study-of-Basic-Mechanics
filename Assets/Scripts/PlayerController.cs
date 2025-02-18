4c3d5abc05703a5644d4d92cea49cf7a607f7afb
Fevzi Bağrıaçık <fevzi.bagriacik1905@gmail.com>
2025-02-18 15:49:33 +0300

    Dash is added and jump is improved.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -13,16 +14,21 @@ public class PlayerController : MonoBehaviour
     public float wallCheckRadius = 0.3f;
     public float wallJumpForce = 10f;
     public float wallSlidingSpeed = 0.2f;
-    public float wallJumpingDirection;
     public float wallJumpingTime = 0.2f;
     public float wallJumpingCounter;
     public float wallJumpingDuration = 0.4f;
+    public float dashPower = 24f;
+    public float dashTime = 0.2f;
+    public float dashingCooldown = 1f;
+    public float direction;
 
     [SerializeField] int jumpCount = 0;
 
     [SerializeField] bool isGrounded;
     [SerializeField] bool isWallSliding;
     [SerializeField] bool isWallJumping;
+    [SerializeField] bool canDash = true;
+    [SerializeField] bool isDashing;
 
     public Transform groundCheck;
     public Transform wallCheck;
@@ -32,6 +38,8 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
+    [SerializeField] private TrailRenderer tr;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -39,6 +47,20 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if(rb.velocity.x < 0f)
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 1;
+        }
+
+        if (isDashing)
+        {
+            return;
+        }
+
         if (isGrounded)
         {
             jumpCount = 0;
@@ -55,6 +77,11 @@ public class PlayerController : MonoBehaviour
         WallSlide();
 
         WallJump();
+
+        if(Input.GetKeyDown(KeyCode.F) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     public void Move()
@@ -114,6 +141,24 @@ public class PlayerController : MonoBehaviour
             wallJumpingCounter = 0f;
             Invoke("StopWallJumping", wallJumpingDuration);
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(direction * dashPower, 0f);
+        tr.emitting = true;
+
+        yield return new WaitForSeconds(dashTime);
+
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
     public void StopWallJumping()
